4686c22a757570b215ac8c4b7190acc9926ba7b6
Vinicius <viniciusf84@hotmail.com>
2024-08-27 01:31:20 +0000

    Create index

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function Home() {
+    const [message, setMessage] = useState('');    
+
+    const addMesage = () => {
+        setMessage(prevState =>  prevState ? `${prevState} tanto` : 'Te amo')
+    }
+
+    const divStyle = {
+        background: "#b5ff33", 
+        display: 'flex', 
+        flexWrap: 'nowrap', 
+        flexDirection: "column", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        height: "100vh"
+    }
+
+    return (
+        <div style={divStyle}>
+            <h1>Oi, neguinha!</h1>
+
+            <button onClick={addMesage}>
+                Clique aqui
+            </button>
+            
+            <p>{message}</p>
+        </div>
+    )
+}
+
+export default Home;
