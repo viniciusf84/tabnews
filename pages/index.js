c111dc69987bc6cc6a3b40ca84ba0bb6eedee6fd
Vinicius <viniciusf84@hotmail.com>
2024-08-29 21:42:06 +0000

    adiciona `.prettier`

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,31 @@
 import { useState } from "react";
 
 function Home() {
-    const [message, setMessage] = useState('');    
+  const [message, setMessage] = useState("");
 
-    const addMesage = () => {
-        setMessage(prevState =>  prevState ? `${prevState} tanto` : 'Te amo')
-    }
+  const addMesage = () => {
+    setMessage((prevState) => (prevState ? `${prevState} tanto` : "Te amo"));
+  };
 
-    const divStyle = {
-        background: "#b5ff33", 
-        display: 'flex', 
-        flexWrap: 'nowrap', 
-        flexDirection: "column", 
-        justifyContent: "center", 
-        alignItems: "center", 
-        height: "100vh"
-    }
+  const divStyle = {
+    background: "#b5ff33",
+    display: "flex",
+    flexWrap: "nowrap",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
 
-    return (
-        <div style={divStyle}>
-            <h1>Oi, neguinha!</h1>
+  return (
+    <div style={divStyle}>
+      <h1>Oi, neguinha!</h1>
 
-            <button onClick={addMesage}>
-                Clique aqui
-            </button>
-            
-            <p>{message}</p>
-        </div>
-    )
+      <button onClick={addMesage}>Clique aqui</button>
+
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default Home;
