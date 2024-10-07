bcfefd627d22b1d37e4f8ddce69bef99e6161775
Vinicius <viniciusf84@hotmail.com>
2024-10-07 22:37:11 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result?.rows);
-  response.status(200).json({ chave: "eu sou acima da média" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname= $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      version: postgresVersion?.rows[0].server_version,
+      max_connections: maxConnections?.rows[0].max_connections,
+      opened_connections: openedConnections.rows[0].count,
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,14 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const paresedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(paresedUpdatedAt);
+
+  expect(responseBody.dependencies.version).toBeDefined();
+  expect(responseBody.dependencies.max_connections).toBeDefined();
+  expect(responseBody.dependencies.opened_connections).toBeDefined();
 });
